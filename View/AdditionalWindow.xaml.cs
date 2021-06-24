8ea2b0f707547e11e3930611db3bc9c26a5bb96e
Forcause <hrishtakyan@inbox.ru>
2021-06-24 16:37:04 +0300

    Refactor .cs file of info window

---
+++ View/AdditionalWindow.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows;
+
+namespace ElCon.View
+{
+    public partial class AdditionalWindow : Window
+    {
+        public AdditionalWindow(Window owner)
+        {
+            DataContext = owner.DataContext;
+            InitializeComponent();
+        }
+    }
+}
